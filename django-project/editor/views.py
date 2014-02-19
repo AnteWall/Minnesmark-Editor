5b9d667891aff113f2fba59cbe5d48a159202705
Richard Wandel <ricwa059@student.liu.se>
2014-02-19 15:21:05 +0100

    Added Editor Page

--- django-project/editor/views.py
+++ django-project/editor/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def render_page(request):
+    return render(request, 'editor/editor.html')
+
