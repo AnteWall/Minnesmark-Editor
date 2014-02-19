ab722ba348c6411676aacfc72b18b0f8ef1322cb
Ante Wall <antewall@gmail.com>
2014-02-19 15:54:35 +0100

    Added Geolocation

--- django-project/static/editor/javascript/mapeditor.js
+++ django-project/static/editor/javascript/mapeditor.js
@@ -1,15 +1,44 @@
 $('document').ready(function(){
+    var map;
+    var browserSupportFlag =  new Boolean();
+    var initialLocation = new google.maps.LatLng(59.319878, 18.065536);
 
     function initialize() {
 	var mapOptions = {
-	    center: new google.maps.LatLng(-34.397, 150.644),
-	    zoom: 8
+	    center: new google.maps.LatLng(59.319878, 18.065536),
+	    zoom: 8,
+            disableDefaultUI: true,
+            zoomControl: true
+
 	};
-	console.log("test");
 	var map = new google.maps.Map(document.getElementById("map-canvas"),
 				      mapOptions);
-	console.log(map);
+        // Try W3C Geolocation (Preferred)
+        if(navigator.geolocation) {
+            browserSupportFlag = true;
+            navigator.geolocation.getCurrentPosition(function(position) {
+                initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+                map.setCenter(initialLocation);
+            }, function() {
+                handleNoGeolocation(browserSupportFlag);
+            });
+        }
+        // Browser doesn't support Geolocation
+        else {
+            browserSupportFlag = false;
+            handleNoGeolocation(browserSupportFlag);
+        }
+
+        function handleNoGeolocation(errorFlag) {
+            if (errorFlag == true) {
+                console.log("Geolocation service failed.");
+            } else {
+                console.log("Your browser doesn't support geolocation. We've placed you in Stockholm.");
+            }
+            map.setCenter(initialLocation);
+        }
     }
     google.maps.event.addDomListener(window, 'load', initialize);
-    console.log("oho");
+    
+    
 });
