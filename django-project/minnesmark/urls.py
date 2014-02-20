b8a8c4d2d5cee01ed9bca67d0cdc49bc27d2038c
Richard Wandel <ricwa059@student.liu.se>
2014-02-20 16:56:13 +0100

    Created initial design

--- django-project/minnesmark/urls.py
+++ django-project/minnesmark/urls.py
@@ -14,6 +14,9 @@ urlpatterns = patterns('',
     #Redirects to Login
     url(r'^$', RedirectView.as_view(url='/accounts/login'),name="index"),
 
+    #Redirects to Editor after login
+    url(r'^accounts/profile/', RedirectView.as_view(url='/editor')),
+
     # Examples:
     #url(r'^/$', 'minnesmark.views.home', name='home'),
     # url(r'^minnesmark/', include('minnesmark.foo.urls')),
@@ -31,7 +34,7 @@ urlpatterns = patterns('',
     url(r'^accounts/register$', register_account,name="register"),
 
     #url for the editor
-    url(r'^editor', render_page),
+    url(r'^editor$', render_page),
 
     # Testing
     #url(r'^date/$', current_datetime)
