21913cfc21ee966cc211a21fe63fbe32b19b4201
Tina Danielsson <tinda344@student.liu.se>
2014-03-13 09:36:01 +0100

    Media files database models created

--- django-project/editor/models.py
+++ django-project/editor/models.py
@@ -1,10 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# Update database with ''.
 
-class Media():
-    path = models.CharField(max_length=100)
-    #settings = #enumrerad lista
-    #type = #enumrerad lista
+class Routes(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=40)
+    active = models.BooleanField(default=False)
 
- 
+class Markers(models.Model):
+    route = models.ForeignKey(Routes, null=True)
+    latitude = models.DecimalField(max_digits=30, decimal_places=25)
+    longitude = models.DecimalField(max_digits=30, decimal_places=25)
+    number = models.IntegerField()
+    index = models.IntegerField()
+
+class Points(models.Model):
+    route = models.ForeignKey(Routes, null=True)
+    latitude = models.DecimalField(max_digits=30, decimal_places=25)
+    longitude = models.DecimalField(max_digits=30, decimal_places=25)
+    index = models.IntegerField()
+
+class Media(models.Model):
+    marker = models.ForeignKey(Markers)
+    name = models.CharField(max_length=40)
+    path = models.CharField(max_length=256)
+    size = models.IntegerField()
+    treasure = models.BooleanField(default=False)
+    panorama = models.BooleanField(default=False)
+
+class Media_Types(models.Model):
+    media = models.ForeignKey(Media)
+    category = models.CharField(max_length=20)
+    filetypes = models.CharField(max_length=255)
+    options = models.CharField(max_length=255)
